b3d1ede39eb83e4cf64b7a78e6fa31aeb76d441d
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2023-11-13 20:43:23 -0300

    Agregado test 'And' y su clase.

---
+++ src/model/And.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class And extends CompositeQuery {
+
+	public And(IQuery query1, IQuery query2) {
+		// TODO Auto-generated constructor stub
+		this.query1 = query1;
+		this.query2 = query2;
+	}
+
+	@Override
+	public List<Viaje> filtrar() {
+		Set<Viaje> setViaje1 = new HashSet<Viaje>();
+		Set<Viaje> setViaje2 = new HashSet<Viaje>();
+		setViaje1.addAll(query1.filtrar());
+		setViaje2.addAll(query2.filtrar());
+		Set<Viaje> setViaje3 = new HashSet<Viaje>(setViaje1);
+		setViaje3.retainAll(setViaje2);
+		return setViaje3.stream().toList();
+	}
+}
---
+++ tests/model/AndTest.java
@@ -0,0 +1,72 @@
+package model;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class AndTest {
+
+	private And	and;
+	private FechaLlegada	critFechaLlegada1;
+	private TerminalDestino	critTerminalDestino1;
+
+	private LocalDate		fecha1;
+	private LocalDate		fecha2;
+	private Terminal		terminal1;
+	private Terminal		terminal2;
+	private Viaje			viaje1;
+	private Viaje			viaje2;
+	private Viaje			viaje3;
+	private List<Viaje> 	viajes = new ArrayList<Viaje>();
+
+	@BeforeEach
+	public void setUp() {
+	// DOC (Depended-On-Component): nuestros doubles
+		this.viaje1 	= spy(Viaje.class);
+		this.viaje2 	= mock(Viaje.class);
+		this.viaje3 	= spy(Viaje.class);
+		viajes.add(viaje1);
+		viajes.add(viaje2);
+		viajes.add(viaje3);
+
+		this.fecha1		= LocalDate.of(1,1,1);
+		this.fecha2		= LocalDate.of(1,3,1);
+		this.terminal1	= mock(Terminal.class);
+		this.terminal2	= mock(Terminal.class);
+
+		this.critFechaLlegada1 		= spy(new FechaLlegada(this.fecha1, this.viajes));
+		this.critTerminalDestino1 	= spy(new TerminalDestino(this.terminal1, this.viajes));
+
+		// SUT (System Under Test): objeto a testear
+		this.and = new And(this.critFechaLlegada1, this.critTerminalDestino1);
+	}
+
+	@Test
+	void testConstructor() {
+		assertNotNull(this.and);
+	}
+
+	@Test
+	public void testAnd2SimpleQuery() {
+		//	Hay que usar HashSet para los tests?
+		when(this.viaje1.getFechaDeLlegada()).thenReturn(fecha1);
+		when(this.viaje2.getFechaDeLlegada()).thenReturn(fecha2);
+		when(this.viaje3.getFechaDeLlegada()).thenReturn(fecha2);
+
+		when(this.viaje1.getTerminalDestino()).thenReturn(terminal1);
+		when(this.viaje2.getTerminalDestino()).thenReturn(terminal2);
+		when(this.viaje3.getTerminalDestino()).thenReturn(terminal1);
+
+		// solo viaje 1 tiene la fecha de llegada y el destino
+
+		assertTrue(and.filtrar().contains(viaje1));
+		assertFalse(and.filtrar().contains(viaje2));
+		assertFalse(and.filtrar().contains(viaje3));
+	}
+}
