584f769ea92a964ee7de6963abfe3f1a6ae3bfb5
Eduardo61Martinez <martin61martinez@hotmail.com.ar>
2023-11-19 17:06:41 -0300

    gps con timer

--- src/model/GPS.java
+++ src/model/GPS.java
@@ -1,28 +1,56 @@
 package model;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Timer;
+import java.util.Random;
 
-public class GPS {
+import java.util.TimerTask;
 
-	private Posicion	posicion;
-	private float		timer;// = new Timer() // TODO
+public class GPS extends TimerTask {
 
-	public GPS() {}
+	private Posicion posicion;
+	private long periodo;
+	private Timer cronometro = new Timer(); 
 	
-	public GPS(float timer) { // FIXME
-		// TODO Auto-generated constructor stub
-		this.timer = timer;
-	}
+	// variables para generar datos de la posición 
+	private Random random = new Random();
+	private List<Integer> diferenciales = Arrays.asList(1, -1, 0);
 
-	public GPS(float timer, Posicion posicion) { // Solo para testear posicion
+
+	public GPS(long periodo, Posicion posicion) {
 		// TODO Auto-generated constructor stub
-		this.timer = timer;
+		this.periodo = periodo;
 		this.posicion = posicion;
+		this.cronometro.schedule(this, 0, this.getPeriodo());
 	}
 
-	public Posicion getPosicion() { return this.posicion; }
-	private void setPosicion(Posicion newPosicion) { this.posicion = newPosicion; }
-	
-	public float getTimer() { return this.timer; }
-	public void setTimer(float newTime) { this.timer = newTime; } //FIXME
+	public Posicion getPosicion() {
+		return this.posicion;
+	}
+
+	private void setPosicion(Posicion newPosicion) {
+		this.posicion = newPosicion;
+	}
+
+	public long getPeriodo() {
+		return this.periodo;
+	}
+
+	public void setPeriodo(long periodoNuevo) {
+		this.periodo = periodoNuevo;
+	} 
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		this.setPosicion(this.obtenerPosicion());
+	}
+
+	public Posicion obtenerPosicion() {
+		int dx = diferenciales.get(random.nextInt(diferenciales.size()));
+		int dy = diferenciales.get(random.nextInt(diferenciales.size()));
+		return new Posicion(posicion.getLatitud() + dx, posicion.getLongitud() + dy);
+	}
+
 }
