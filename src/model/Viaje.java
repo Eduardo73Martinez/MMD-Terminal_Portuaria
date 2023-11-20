477cd4cf9fe4daa8eee99bead72b17bb9c333188
Eduardo61Martinez <martin61martinez@hotmail.com.ar>
2023-11-19 23:25:24 -0300

    modelo de viaje creado

--- src/model/Viaje.java
+++ src/model/Viaje.java
@@ -1,30 +1,66 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+/**
+ * 
+ * @author Martinez Eduardo
+ *
+ */
 public class Viaje {
-
 	private LocalDate fechaDeSalida;
-	private LocalDate fechaDeLlegada;
+	private CircuitoMaritimo recorrido;
+	private Buque buque;
+	private List<Tramo> tramos;
+	// suponemos que el viaje un recorrido de tramos de un circuito. Puede ser un
+	// circuito completo si lo desea.
+
+	public Viaje(LocalDate fechaDeSalida, List<Tramo> tramos, CircuitoMaritimo circuito, Buque buque) {
+		this.fechaDeSalida = fechaDeSalida;
+		this.tramos = tramos;
+		this.recorrido = circuito;
+		this.buque = buque;
+		this.validarViaje(tramos);
+	}
 
 	public LocalDate getFechaDeSalida() {
 		// TODO Auto-generated method stub
 		return this.fechaDeSalida;
 	}
 
-	public Object getFechaDeLlegada() {
+	public LocalDate getFechaDeLlegada() {
 		// TODO Auto-generated method stub
-		return this.fechaDeLlegada;
+		int diasDeViaje = this.tramos.stream().mapToInt(s -> s.getTiempo()).sum();
+		return this.fechaDeSalida.plusDays(diasDeViaje);
 	}
 
-	public Object getTerminalDestino() {
+	public Terminal getTerminalDestino() throws ArrayIndexOutOfBoundsException {
 		// TODO Auto-generated method stub
-		return null;
+		try {
+			return this.tramos.get(tramos.size() - 1).getOrigen();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("No hay terminal destino en el viaje");
+		}
 	}
 
-	public Object getTerminalOrigen() {
+	public Terminal getTerminalOrigen() throws ArrayIndexOutOfBoundsException {
 		// TODO Auto-generated method stub
-		return null;
+		try {
+			return this.tramos.get(0).getOrigen();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("No hay terminal origen en el viaje");
+		}
+	}
+
+	public void validarViaje(List<Tramo> tramos) {
+		if (!this.recorrido.getTramos().containsAll(tramos)) {
+			throw new TramoExceptions("El viaje completo no está en el circuito dado");
+		}
+	}
+
+	public double getCosto() {
+		return this.tramos.stream().mapToDouble(s -> s.getPrecio()).sum();
 	}
 
 }
---
+++ tests/model/ViajeTest.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Martinez Eduardo
+ *
+ */
+class ViajeTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	
+	@Test
+	void testViaje() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	void testGetFechaDeSalida() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	void testGetFechaDeLlegada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTerminalDestino() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTerminalOrigen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidarViaje() {
+		fail("Not yet implemented");
+	}
+	@Test 
+	void getCosto() {
+		fail("Not yet implemented");
+	}
+
+}
